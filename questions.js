4910928b28a648bb8ff742f534c572bdc588a792
goeringkenneth <71526592+goeringkenneth@users.noreply.github.com>
2020-12-04 09:57:56 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,87 @@
+var questions={
+    position:0,
+    qustionlist:[
+        {
+
+        question:"q1",
+        choices:["w","w","c","w",
+
+        ],
+        answer:3
+    },
+    {
+
+        question:"q2",
+        choices:["w","w","w","c",
+
+        ],
+        answer:4
+    },
+    {
+
+        question:"q3",
+        choices:["w","c","w","w",
+
+        ],
+        answer:2
+    },
+    {
+
+        question:"q4",
+        choices:["c","w","w","w",
+
+        ],
+        answer:1
+    },
+    {
+
+        question:"q5",
+        choices:["w","w","w","c",
+
+        ],
+        answer:4
+    },
+    {
+
+        question:"q6",
+        choices:["c","w","w","w",
+
+        ],
+        answer:1
+    },
+    {
+
+        question:"q7",
+        choices:["w","c","w","w",
+
+        ],
+        answer:2
+    },
+    {
+
+        question:"q8",
+        choices:["c","w","w","w",
+
+        ],
+        answer:1
+    },
+    {
+
+        question:"q9",
+        choices:["w","w","w","c",
+
+        ],
+        answer:4
+    },
+    {
+
+        question:"q10",
+        choices:["w","c","w","w",
+
+        ],
+        answer:2
+    },
+    ]
+};
+
+// run questions with index
